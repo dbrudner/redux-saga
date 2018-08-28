2e35c22c487bd99ecb0991b9a63a38b9c0eca924
dbrudner <32269552+dbrudner@users.noreply.github.com>
2018-08-28 12:32:58 -0500

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
 
-const logo = require('./logo.svg');
+const logo = require("./logo.svg");
 
 class App extends React.Component {
   render() {
